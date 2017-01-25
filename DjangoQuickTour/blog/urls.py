c27bf6ea0518cce801efaf787534ff3244c55ccd
Ming Yeh <mingyeh1979@hotmail.com>
2017-01-25 14:53:25 +0800

    Note 5: Display article in individual page. Add pagination to index page.

--- DjangoQuickTour/blog/urls.py
+++ DjangoQuickTour/blog/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index')
+    url(r'^$', views.index, name = 'index'),
+    url(r'^(?P<pageNo>[\d]+)',views.indexInPages, name = 'indexInPages'),
+    url(r'^article/(?P<articleID>[\d]+)', views.article, name='article'),
 ]
--- DjangoQuickTour/blog/views.py
+++ DjangoQuickTour/blog/views.py
@@ -3,11 +3,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from blog.models import *
+from django.core.paginator import Paginator
 
-def index(reqeust):
+def index(request):
+    return indexInPages(request,1)
+
+def article(request, articleID):
+    template = loader.get_template('blog/article.html')
+    article = Article.objects.get(id = articleID)
+    context = {
+        'article': article,
+    }
+    return HttpResponse(template.render(context, request))
+
+def indexInPages(request, pageNo):
     template = loader.get_template('blog/index.html')
     allArticles = Article.objects.all().order_by('-timestamp')
+    p = Paginator(allArticles, 5)
+    articlesInPage = p.page(pageNo)
     context = {
-        'all_articles' : allArticles,
+        'all_articles' : articlesInPage,
     }
-    return HttpResponse(template.render(context, reqeust))
+    return HttpResponse(template.render(context, request))
