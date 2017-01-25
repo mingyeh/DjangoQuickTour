835a33f3cb58bade1b65c3885fb7b878e43c0e32
Ming Yeh <mingyeh1979@hotmail.com>
2017-01-25 10:39:38 +0800

    Note 4: Fill page template with data in database. Add static file directory.

--- DjangoQuickTour/blog/views.py
+++ DjangoQuickTour/blog/views.py
@@ -2,10 +2,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from blog.models import *
 
 def index(reqeust):
     template = loader.get_template('blog/index.html')
+    allArticles = Article.objects.all().order_by('-timestamp')
     context = {
-        'authorName' : '大狗',
+        'all_articles' : allArticles,
     }
     return HttpResponse(template.render(context, reqeust))
